c5ab6a8e72d4d92639d5e4523092c9ac839fda41
chamaboo2 <qx3luvv@gmail.com>
2026-08-30 10:16:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,18 @@ st.set_page_config(
 
 st.title("おしコンパス 🧭")
 st.write("好きな場所は、あっち！")
+
+st.divider()
+
+st.subheader("聖地を探す")
+
+place_name = st.text_input(
+    "場所の名前",
+    placeholder="例：東京タワー、東京駅、秋葉原"
+)
+
+if st.button("検索"):
+    if place_name:
+        st.write(f「検索する場所：{place_name}」)
+    else:
+        st.warning("場所の名前を入力してください")
